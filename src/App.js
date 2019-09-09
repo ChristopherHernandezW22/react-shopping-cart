8609c46bdc42c0032e9781c7cc5b36f46a0f1a75
Christopher Hernandez <cerberean661@gmail.com>
2019-09-09 01:59:43 -0700

    import ProductContext & adding value in Products.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+import ProductContext from './contexts/ProductContext';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
@@ -16,6 +18,7 @@ function App() {
 	};
 
 	return (
+		<ProductContext.Provider value={{ products, addItem }} >
 		<div className="App">
 			<Navigation cart={cart} />
 
@@ -23,12 +26,13 @@ function App() {
 			<Route
 				exact
 				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
+				component={Products}
+				// render={() => (
+				// 	<Products
+				// 		products={products}
+				// 		addItem={addItem}
+				// 	/>
+				// )}
 			/>
 
 			<Route
@@ -36,6 +40,7 @@ function App() {
 				render={() => <ShoppingCart cart={cart} />}
 			/>
 		</div>
+		</ ProductContext.Provider>
 	);
 }
 
